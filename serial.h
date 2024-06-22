eb01b609143e865f340556d7c59922ab3535e789
Dane Wagner <dane.wagner@gmail.com>
2024-06-22 15:33:32 -0500

    Split hardware-specific definitions to separate header

--- serial.h
+++ serial.h
@@ -7,10 +7,9 @@
 #define SERIAL_H
 
 void initializeSerial();
-void writeCharToSerial(const char c)
+void writeCharToSerial(const char c);
 void writeStringToSerial(const char *buffer, const int length);
 char readSerial();
 int readFromSerial(char *buffer, const int size);
-
 
 #endif //SERIAL_H
