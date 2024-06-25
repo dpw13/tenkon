52f208f23018df79297fc59fcef6a66be6ec54ec
Dane Wagner <dane.wagner@gmail.com>
2024-06-25 12:29:24 -0500

    Add memtest and context info print to console

--- console.c
+++ console.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
 #include "serial.h"
 
 void memWrite(const char b, const int addr) {
@@ -39,7 +41,63 @@ void showData(int addr, const int size) {
     }
 }
 
-void consoleLoop() {
+/* Note that this overwrites values in memory in the specified window, so
+ * avoid calling this with a window that overlaps the current stack.
+ */
+void memTest(const int addr, const int size) {
+    uint32_t *buf;
+    uint32_t offset;
+    uint32_t errCount;
+
+    /* Seed PRNG so we always get the same sequence */
+    srand(0);
+    buf = (uint32_t *)addr;
+    for (offset = 0; offset < size; offset += sizeof(uint32_t)) {
+        *buf++ = rand();
+    }
+
+    /* Reset PRNG sequence */
+    srand(0);
+    buf = (uint32_t *)addr;
+    errCount = 0;
+    for (offset = 0; offset < size; offset += sizeof(uint32_t)) {
+        uint32_t val = rand();
+        if (*buf != val) {
+            /* Only print first error */
+            if (errCount == 0) {
+                printf("Mismatch at %p: expected %08x but read %08x. Supressing further error messages.\n", buf, val, *buf);
+            }
+            errCount += 1;
+        }
+    }
+
+    printf("Found %d errors\n", errCount);
+}
+
+/* Print out some minimal info about the current execution context
+ * Useful for verifying supervisor mode, interrupt mask, and current
+ * code and stack locations.
+ */
+void contextInfo(void) {
+    uint16_t statusReg;
+    void *retAddr = __builtin_return_address(0);
+    void *framePtr;
+    void *stackPtr;
+
+    asm volatile ("movew %%sr, %0"
+        : "=r" (statusReg));
+    asm volatile ("movel %%fp, %0"
+        : "=r" (framePtr));
+    asm volatile ("movel %%sp, %0"
+        : "=r" (stackPtr));
+    
+    printf("Caller: %p\n", retAddr);
+    printf("FP: %p\n", framePtr);
+    printf("SP: %p\n", stackPtr);
+    printf("SR: %04hx\n", statusReg);
+}
+
+void consoleLoop(void) {
     char cmd;
     int addr = 0;
     int size = 0;
@@ -68,6 +126,16 @@ void consoleLoop() {
                 scanf("%x", &addr);
                 run(addr);
                 break;
+            case 'M': //Memtest
+            case 'm':
+                scanf("%x", &addr);
+                scanf("%d", &size);
+                memTest(addr, size);
+                break;
+            case 'i': //Context info
+            case 'I':
+                contextInfo();
+                break;
             default:
                 printf("Unrecognized Command: %c\n", cmd);
                 break;
@@ -85,6 +153,8 @@ int main(void) {
     printf("              Continues to load bytes until you send Z.\n");
     printf("P addr size - Peeks at size bytes at addr.\n");
     printf("R addr      - Calls subroutine located at addr\n");
+    printf("M addr size - Test size bytes of memory starting at addr\n");
+    printf("I           - Print info about the current execution context\n");
     consoleLoop();
     return 0;
 }
