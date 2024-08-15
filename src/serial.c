e2cf2d2d9743d9e7c99b08a9a0f8c82424927d78
Dane Wagner <dane.wagner@gmail.com>
2024-08-15 17:55:34 -0500

    Move to second UART port

--- src/serial.c
+++ src/serial.c
@@ -11,26 +11,34 @@
 #define rtc_regs ((void *)0xFE800000)
 #define ps2_regs ((void *)0xFEC00000)
 
+/* Change these defines to change the active console */
+#define console_regs (term_regs)
+#define console_port (console_regs->B)
+
 void initializeSerial() {
-    term_regs->A.CR = CR_CMD_RESET_TX;
-    term_regs->A.CR = CR_CMD_RESET_RX;
-    term_regs->A.CR = CR_CMD_RESET_MR0;
-    term_regs->ACR = ACR_BAUDGEN_TABLE_0 | ACR_CTMODE_COUNTER_IP2;
-    term_regs->A.CSR = CSR_BAUD_TX(CSR_BAUD_EXT2_115_2K) | CSR_BAUD_RX(CSR_BAUD_EXT2_115_2K);
-    term_regs->A.MR = MR0_DIS_WDT | MR0_TX_FULL_THRESH_4 | MR0_BAUD_EXT2;
-    term_regs->A.MR = MR1_EN_RX_RTS | MR1_NO_PARITY | MR1_BPC_8;
-    term_regs->A.MR = MR2_DIS_TX_RTS | MR2_DIS_CTS_TX | MR2_STOP_LEN_1_00;
-    term_regs->A.CR = CR_CMD_NONE | CR_ENA_RX | CR_ENA_TX;
+    /* The baud rate selection is specific to channel A */
+    console_regs->A.CR = CR_CMD_RESET_MR0;
+    console_regs->A.MR = MR0_DIS_WDT | MR0_TX_FULL_THRESH_4 | MR0_BAUD_EXT2;
+    console_regs->ACR = ACR_BAUDGEN_TABLE_0 | ACR_CTMODE_COUNTER_IP2;
+
+    console_port.CR = CR_CMD_RESET_TX;
+    console_port.CR = CR_CMD_RESET_RX;
+    console_port.CR = CR_CMD_RESET_MR0;
+    console_port.CSR = CSR_BAUD_TX(CSR_BAUD_EXT2_115_2K) | CSR_BAUD_RX(CSR_BAUD_EXT2_115_2K);
+    console_port.MR = MR0_DIS_WDT | MR0_TX_FULL_THRESH_4 | MR0_BAUD_EXT2;
+    console_port.MR = MR1_EN_RX_RTS | MR1_NO_PARITY | MR1_BPC_8;
+    console_port.MR = MR2_DIS_TX_RTS | MR2_DIS_CTS_TX | MR2_STOP_LEN_1_00;
+    console_port.CR = CR_CMD_NONE | CR_ENA_RX | CR_ENA_TX;
 }
 
 void forceWriteSerial(const char c) {
-    term_regs->A.TxFIFO = c;
+    console_port.TxFIFO = c;
 }
 
 static inline void _writeSerial(const char c) {
     // Busy wait until ready...probably not the best
-    while( !(term_regs->A.SR & SR_TXRDY) ) {}
-    term_regs->A.TxFIFO = c;
+    while( !(console_port.SR & SR_TXRDY) ) {}
+    console_port.TxFIFO = c;
 }
 
 void writeSerial(const char c) {
@@ -49,23 +57,23 @@ void writeStringToSerial(const char *buffer, const int length) {
 }
 
 unsigned char serialGetError(void) {
-    uint8_t ret = term_regs->A.SR & SR_ERR_MASK;
+    uint8_t ret = console_port.SR & SR_ERR_MASK;
     // Clear errors
-    term_regs->A.CR = CR_CMD_RESET_ERR;
+    console_port.CR = CR_CMD_RESET_ERR;
     return ret;
 }
 
 char readSerial(void) {
-    while( !(term_regs->A.SR & SR_RXRDY) ) {}
-    return term_regs->A.RxFIFO;
+    while( !(console_port.SR & SR_RXRDY) ) {}
+    return console_port.RxFIFO;
 }
 
 int readFromSerial(char *buffer, const int size) {
     for(int i = 0; i < size; i++) {
-        if( !(term_regs->A.SR & SR_RXRDY) ) {
+        if( !(console_port.SR & SR_RXRDY) ) {
             return i;
         }
-        buffer[i] = term_regs->A.RxFIFO;
+        buffer[i] = console_port.RxFIFO;
     }
     return size;
 }
